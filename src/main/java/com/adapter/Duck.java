cf593e63de5eb579b3a9bc8cf89ed4cbe478d973
boram.chang <boram.chang@navercorp.com>
2024-01-31 09:58:31 +0900

    add adapter

--- src/main/java/com/adapter/Duck.java
+++
@@ -1,7 +0,0 @@
-package com.adapter;
-
-public interface Duck {
-    void guack();
-    void fly();
-
-}
--- src/main/java/com/adapter/Turkey.java
+++
@@ -1,6 +0,0 @@
-package com.adapter;
-
-public interface Turkey {
-    void gobble();
-    void fly();
-}
